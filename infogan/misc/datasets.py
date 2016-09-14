228bc956103675223ea817ee2ad2ec02c3d65687
lmj <lmj@lmjdeMacBook-Air.local>
2016-09-14 18:51:19 +0800

    9.14.200 run ok,pbar ok,change size to 178218

--- infogan/misc/datasets.py
+++ infogan/misc/datasets.py
@@ -85,7 +85,8 @@ class MnistDataset(object):
         self.test = dataset.test
         self.validation = dataset.validation
         self.image_dim = 28 * 28
-        self.image_shape = (28, 28, 1)
+        #self.image_shape = (28, 28, 1)
+        self.image_shape = (178, 218, 1)
 
     def transform(self, data):
         return data
