2ec8f46e38813a6d4997316d96b7783b92927a94
lmj <lmj@lmjdeMacBook-Air.local>
2016-09-16 23:28:52 +0800

    9.16.2 gorun snapshot

--- infogan/algos/infogan_trainer.py
+++ infogan/algos/infogan_trainer.py
@@ -25,7 +25,7 @@ class InfoGANTrainer(object):
                  checkpoint_dir="ckt",
                  max_epoch=100,
                  updates_per_epoch=100,
-                 snapshot_interval=47,
+                 snapshot_interval=97,
                  info_reg_coeff=1.0,
                  discriminator_learning_rate=2e-4,
                  generator_learning_rate=2e-4,
