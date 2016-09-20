10c0c5bb0a93d5dbbadaa6b5000b5be490df387d
lmj <lmj@lmjdeMacBook-Air.local>
2016-09-20 16:59:42 +0800

    9.20..2 debug  change model conv deconv dis gan model arch

--- infogan/models/regularized_gan.py
+++ infogan/models/regularized_gan.py
@@ -82,20 +82,21 @@ class RegularizedGAN(object):
                      #reshape([-1, image_size / 4, image_size / 4, 128]).
 
 
-                     custom_fully_connected(image_size / 8 * image_size / 8 * 256).
+                     custom_fully_connected(image_size / 8 * image_size / 8 * 512).
                      fc_batch_norm().
                      apply(tf.nn.relu).
-                     reshape([-1, image_size / 8, image_size / 8, 256]).
+                     reshape([-1, image_size / 8, image_size / 8, 512]).
+                     
+
+                     custom_deconv2d([0, image_size / 8, image_size / 8, 256], k_h=4, k_w=4).
+                     conv_batch_norm().
+                     apply(tf.nn.relu).
 
                      custom_deconv2d([0, image_size / 4, image_size / 4, 128], k_h=4, k_w=4).
                      conv_batch_norm().
                      apply(tf.nn.relu).
 
-                     #custom_deconv2d([0, image_size / 8, image_size / 8, 64], k_h=4, k_w=4).
-                     #conv_batch_norm().
-                     #apply(tf.nn.relu).
-
-
+                     
                      custom_deconv2d([0, image_size / 2, image_size / 2, 64], k_h=4, k_w=4).
                      conv_batch_norm().
                      apply(tf.nn.relu).
