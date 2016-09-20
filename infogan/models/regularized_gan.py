3b2ff82642168e26f63a140291f4524b70803964
lmj <lmj@lmjdeMacBook-Air.local>
2016-09-20 15:23:36 +0800

    9.20..2 debug  change model conv deconv dis gan model arch

--- infogan/models/regularized_gan.py
+++ infogan/models/regularized_gan.py
@@ -75,29 +75,36 @@ class RegularizedGAN(object):
                      custom_fully_connected(1024).
                      fc_batch_norm().
                      apply(tf.nn.relu).
-                     custom_fully_connected(image_size / 32 * image_size / 32 * 1024).
+
+
+                     ##custom_fully_connected(image_size / 32 * image_size / 32 * 1024).
+                     #fc_batch_norm().
+                     #apply(tf.nn.relu).
+
+                     #reshape([-1, image_size / 32, image_size / 32, 1024]).
+
+                     #custom_deconv2d([0, image_size / 16, image_size / 16, 512], k_h=4, k_w=4).
+                     #conv_batch_norm().
+                     #apply(tf.nn.relu).
+
+                     #custom_deconv2d([0, image_size / 8, image_size / 8, 256], k_h=4, k_w=4).
+                     #conv_batch_norm().
+                     #apply(tf.nn.relu).
+
+
+
+                     custom_fully_connected(image_size / 4 * image_size / 4 * 128).
                      fc_batch_norm().
                      apply(tf.nn.relu).
-
-                     
-                     reshape([-1, image_size / 32, image_size / 32, 1024]).
-
-                     custom_deconv2d([0, image_size / 16, image_size / 16, 512], k_h=4, k_w=4).
-                     conv_batch_norm().
-                     apply(tf.nn.relu).
-
-                     custom_deconv2d([0, image_size / 8, image_size / 8, 256], k_h=4, k_w=4).
-                     conv_batch_norm().
-                     apply(tf.nn.relu).
+                     reshape([-1, image_size / 4, image_size / 4, 128]).
 
 
                      custom_deconv2d([0, image_size / 2, image_size / 2, 64], k_h=4, k_w=4).
                      conv_batch_norm().
                      apply(tf.nn.relu).
                      custom_deconv2d([0] + list(image_shape), k_h=4, k_w=4).
-
-
                      flatten())
+
         else:
             raise NotImplementedError
 
