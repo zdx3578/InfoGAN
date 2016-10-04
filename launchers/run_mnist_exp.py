bd3b68b68c321753b38e6e6b2b76a890a8f96e2e
lmj <lmj@lmjdeMacBook-Air.local>
2016-10-04 18:59:43 +0800

    10.4.18:39

--- launchers/run_mnist_exp.py
+++ launchers/run_mnist_exp.py
@@ -22,14 +22,14 @@ if __name__ == "__main__":
     
     #!!!!!
     #batch_size = 128
-    batch_size = 200
+    batch_size = 128
     #updates_per_epoch = 50
     updates_per_epoch = 100
     max_epoch = 301
     #max_epoch = 201
     #snapshot_interval = 5000
     snapshot_interval = 10000
-    ganlp=1
+    ganlp=2
     #ganlp=4
 
 
