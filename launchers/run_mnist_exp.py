d03d5538cf381e4dcdac240c430b9e8dcb501054
lmj <lmj@lmjdeMacBook-Air.local>
2016-10-10 20:12:54 +0800

    10.10 while train generate net until ok;dim 32 32 3

--- launchers/run_mnist_exp.py
+++ launchers/run_mnist_exp.py
@@ -24,7 +24,7 @@ if __name__ == "__main__":
     #batch_size = 128
     batch_size = 128
     #updates_per_epoch = 50
-    updates_per_epoch = 25
+    updates_per_epoch = 5
     max_epoch = 301
     #max_epoch = 201
     #snapshot_interval = 5000
